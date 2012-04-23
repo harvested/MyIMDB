ddc2d3e050d231ee28c0fafaf05a3e1ca4e42b3d
Petrea Corneliu Stefan <stefan.petrea@gmail.com>
2012-04-23 22:58:56 +0300

    fixed the setRank redirect problem, default rank is displayed

--- lib/MyIMDB/Models/Base.pm
+++ lib/MyIMDB/Models/Base.pm
@@ -5,6 +5,6 @@ use warnings;
 
 use base 'Class::DBI::mysql';
 
-MyIMDB::Models::Base->connection('DBI:mysql:database=MyIMDB;host=127.0.0.1','root','as')
+MyIMDB::Models::Base->connection('DBI:mysql:database=MyIMDB;host=127.0.0.1','root','smeker')
 								or die "couldn't connect to db $!\n";
 1;
