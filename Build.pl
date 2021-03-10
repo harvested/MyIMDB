eada3639f60853bc87a0cc1bff324470d66ce2f6
Victor Andrei Doanca <victor@MacBook-Pro.local>
2021-03-10 15:37:56 +0100

    updated Build.PL

--- Build.pl
+++ Build.pl
@@ -1,12 +1,10 @@
 use Module::Build;
 
-	Module::Build->new(
-		module_name => 'MyIMDB',
-		license => 'perl',
-		dist_abstract => 'test tets',
-		requires    => { 'Mojolicious'	=> '9.03',
+Module::Build->new(
+	module_name		=> 'MyIMDB',
+	license			=> 'perl',
+	dist_abstract	=> 'A pet project that works as IMDB.com',
+	requires    	=> { 'Mojolicious'	=> '9.03',
 						 'Rose::DB'		=> '0.783',
-						 'Roso::DB::Object' => '0.819',
 						},
-
-	)->create_build_script;
+)->create_build_script;
