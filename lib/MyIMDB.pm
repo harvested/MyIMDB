68137f0fec0ad29ba6221702903ea1c0872d4934
Victor Andrei Doanca <victor@MacBook-Pro.local>
2021-03-10 14:27:09 +0100

    replace old routes() method with new get()

--- lib/MyIMDB.pm
+++ lib/MyIMDB.pm
@@ -9,7 +9,7 @@ sub startup {
     my $self = shift;
 
     # Documentation browser under "/perldoc"
-    $self->plugin('PODRenderer');
+    # $self->plugin('PODRenderer');
 
     # Routes
     my $r = $self->routes;
@@ -29,32 +29,32 @@ sub startup {
   $r->post('/join')->to('users#join');
 
   # Actors routes
-  my $actor = $r->route('/actors/:id')->to(controller => 'actors');
-  $actor->route('/')->to(action => 'details');
+  my $actor = $r->get('/actors/:id')->to(controller => 'actors');
+  $actor->get('/')->to(action => 'details');
   
   # we create an routing bridge (Mojo 6: under) to check if the user is logged in or not
   $actor->under('/')->to('users#auth')->post('/mark')->to('actors#markFavorite');
 
   # Movies routes
-  my $movie = $r->route('/movies/:id')->to(controller => 'movies');
-  $movie->route('/')->to(action => 'details');
-  $movie->route('/buy')->to('basket#buyMovie');
+  my $movie = $r->get('/movies/:id')->to(controller => 'movies');
+  $movie->get('/')->to(action => 'details');
+  $movie->get('/buy')->to('basket#buyMovie');
   
   $movie->under('/')->to('users#auth')->post('/rate')->to('movies#setRate');
   $movie->under('/')->to('users#auth')->post('/mark')->to('movies#markFavorite');
   $movie->under('/')->to('users#auth')->post('/comment')->to('movies#comment');
 
   # Users routes
-  $r->route('/user/#user_name')->to('users#home');
+  $r->get('/user/#user_name')->to('users#home');
 
   # Basket routes
-  my $basket = $r->route('/basket')->to(controller => 'basket');
-  $basket->route('/view')->to(action => 'view');
-  $basket->route('/update')->to(action => 'update');
-  $basket->route('/empty')->to(action => 'empty');
-  $basket->route('/delete/:id')->to(action => 'delete');
-  $basket->route('/checkout')->to(action => 'checkout');
-  $basket->route('/send')->to(action =>'sendEmail');
+  my $basket = $r->get('/basket')->to(controller => 'basket');
+  $basket->get('/view')->to(action => 'view');
+  $basket->get('/update')->to(action => 'update');
+  $basket->get('/empty')->to(action => 'empty');
+  $basket->get('/delete/:id')->to(action => 'delete');
+  $basket->get('/checkout')->to(action => 'checkout');
+  $basket->get('/send')->to(action =>'sendEmail');
 
   # Admin routes
   $r->get('/admin_login')->to(template => 'admins/login');
@@ -63,15 +63,15 @@ sub startup {
   # this bridge (Mojo 6: under) is used to always check if and admin is logged in or not
   # so that the actions below will execute or...not 
   my $admin = $r->under('/admin')->to('admins#auth');
-  $admin->route('/')->to(controller => 'admins');
-  $admin->route('/#admin_name')->to(action => 'home');
-  $admin->route('/users/all')->to(action => 'allUsers');
-  $admin->route('/users/:id/delete')->to(action => 'deleteUser');
-  $admin->route('/users/search')->to(action => 'searchUsers');
-  $admin->route('/users/#user_name')->to(action => 'userDetails');
+  $admin->get('/')->to(controller => 'admins');
+  $admin->get('/#admin_name')->to(action => 'home');
+  $admin->get('/users/all')->to(action => 'allUsers');
+  $admin->get('/users/:id/delete')->to(action => 'deleteUser');
+  $admin->get('/users/search')->to(action => 'searchUsers');
+  $admin->get('/users/#user_name')->to(action => 'userDetails');
 
   # Error routes
-  $r->route('/404')->to(template => 'errors/404');
+  $r->get('/404')->to(template => 'errors/404');
 }
 
 1;
