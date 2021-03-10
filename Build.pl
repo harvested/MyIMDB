b691b6ba5913305db2339f1dc234b14e70184d54
Victor Andrei Doanca <victor@MacBook-Pro.local>
2021-03-10 15:33:51 +0100

    added requirements

--- Build.pl
+++ Build.pl
@@ -1,7 +1,12 @@
- use Module::Build;
+use Module::Build;
 
-  Module::Build->new
-      ( module_name => 'MyIMDB',
-        license => 'perl',
-	dist_abstract => 'test tets'
-      )->create_build_script;
+	Module::Build->new(
+		module_name => 'MyIMDB',
+		license => 'perl',
+		dist_abstract => 'test tets',
+		requires    => { 'Mojolicious'	=> '9.03',
+						 'Rose::DB'		=> '0.783',
+						 'Roso::DB::Object' => '0.819',
+						},
+
+	)->create_build_script;
