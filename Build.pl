19614e530a2b097f9af6fa3cb7403c0912293b05
Victor Andrei Doanca <victor@MacBook-Pro.local>
2021-03-10 16:24:32 +0100

    added dependency

--- Build.pl
+++ Build.pl
@@ -4,7 +4,8 @@ Module::Build->new(
 	module_name		=> 'MyIMDB',
 	license			=> 'perl',
 	dist_abstract	=> 'A pet project that works as IMDB.com',
-	requires    	=> { 'Mojolicious'	=> '9.03',
-						 'Rose::DB'		=> '0.783',
+	requires    	=> { 'Mojolicious'			=> '9.03',
+						 'Rose::DB'				=> '0.783',
+						 'Rose::DB::Object'		=> '0.819',
 						},
 )->create_build_script;
