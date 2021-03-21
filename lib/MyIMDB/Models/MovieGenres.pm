8379668a04b9b46341e19c1765183826fa36965c
Victor Andrei Doanca <andrei_doanca@yahoo.com>
2021-03-21 13:34:17 +0100

    cleaner sintax

--- lib/MyIMDB/Models/MovieGenres.pm
+++ lib/MyIMDB/Models/MovieGenres.pm
@@ -3,10 +3,10 @@ use Mojo::Base -strict;
 use parent 'MyIMDB::Models::Object';
 
 __PACKAGE__->set_up_table(
-    table => 'movies_genres',
-    columns => [ qw(movie_id genre_id) ],
-    pk_columns => [ qw(movie_id genre_id) ],
-    foreign_keys => [ qw(movies genres) ],
+    table        => 'movies_genres',
+    columns      => ['movie_id', 'genre_id'],
+    pk_columns   => ['movie_id', 'genre_id'],
+    foreign_keys => ['movies', 'genres'],
 );
 
 #__PACKAGE__->has_a( movie_id => 'MyIMDB::Models::Movies' );
